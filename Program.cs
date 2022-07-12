cdaf48fc11711c4557cd246d26882120256ec763
25081997pc <chavanpooja2508@gmail.com>
2022-07-12 18:09:59 +0530

    Calling Method

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Line Comparision Computation Program");
+
             MeasureLineLength.MeasuretheLength();
             
         }
