3490665844f939bf4313faee88b0952453bc0cf3
25081997pc <chavanpooja2508@gmail.com>
2022-07-12 17:37:21 +0530

    Welcome message

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LineComputation
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Line Comparision Computation Program");
+            
+        }
+    }
+}
