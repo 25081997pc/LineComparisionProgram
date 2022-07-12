91f9834c9a32f95af1f014b4cc539d2df2454252
25081997pc <chavanpooja2508@gmail.com>
2022-07-12 17:43:40 +0530

    Calculating the lenth of line

---
+++ CalculateLength.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComputation
+{
+    public class CalculateLength
+    {
+        public static void get()
+        {
+            Console.WriteLine("Enter X start coordinate number of line1 i.e x1 : ");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Y start coordinate number of line1 i.e y1 : ");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter X end coordinate number of line1 i.e x2 : ");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Y end coordinate number of line1 i.e y2 : ");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            CalculatetheLength(x1, y1, x2, y2);
+
+        }
+        public static void CalculatetheLength(int x1, int y1, int x2, int y2)
+        {
+            double lengthofLine = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine(lengthofLine);
+        }
+    }
+}
